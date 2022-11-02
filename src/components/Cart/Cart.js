6d23eaf43a59392b6be7c303c28438bad4224014
AbdallahRaslan <raslangi@gmail.com>
2022-11-02 22:19:37 +0200

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,106 @@
+import React, { useContext, useState } from 'react';
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import CartContext from '../../store/Cart-Context';
+import Checkout from './Checkout';
+import styles from './Cart.module.css';
+
+const Cart = props => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = item => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async userDate => {
+    setIsSubmitting(true);
+    await fetch(
+      'https://react-http-65b4b-default-rtdb.firebaseio.com/orders.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({ user: userDate, orderedItems: cartCtx.items }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
+
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {cartCtx.items.map(item => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+
+  const modalActions = (
+    <div className={styles.actions}>
+      <button className={styles['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={styles.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <React.Fragment>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data ...</p>;
+
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Successfully Sent the order!</p>
+      <div className={styles.actions}>
+        <button className={styles.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}{' '}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
+    </Modal>
+  );
+};
+
+export default Cart;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import HeaderCartButton from './HeaderCartButton';
+import mealsImg from '../../assets/meals.jpg';
+import styles from './Header.module.css';
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={styles['main-image']}>
+        <img src={mealsImg} alt="A table full of food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
